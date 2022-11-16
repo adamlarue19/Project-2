8fe4ef1e42c7ff9bd76ffec8f5eebf6197129ade
Ian <0306pitt@gmail.com>
2022-11-15 19:06:12 -0500

    3 cards added and rowed

--- Main/public/js/googlemaps.js
+++ Main/public/js/googlemaps.js
@@ -20,8 +20,12 @@ function initMap() {
     });
 
      const drag = google.maps.event.addListener(marker,'dragged', () => {
+      google.maps.event.addListener(marker, 'dragend', function (event) {
+        document.getElementById("latbox").value = this.getPosition().lat();
+        document.getElementById("lngbox").value = this.getPosition().lng();
+    });
 
-      console.log(lat)
+      console.log(lat, lng)
     });
     console.log(drag)
   };
